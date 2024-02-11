d465abf5d4c9be6498260ffb4e879aabf9250e19
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-02-11 22:02:51 +0100

    update

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,21 +1,40 @@
-import React from "react"
+import React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: { straggerChildren: 0.6 },
+  },
+  closed: {
+    transition: { straggerChildren: 0.05, straggerDirection: -1 },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {delay: 0.4}
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {delay: 0.4}
+  },
+};
 
 const Links = () => {
-  const items = [
-    "Homepage",
-    "Services", 
-    "Portfolio",
-    "Contact", 
-    "About"
-  ]
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
 
   return (
-    <div className='links'>
-        {items.map(item => (
-            <a href={`#${item}`} key={item}>{item}</a>
-        ))}
-    </div>
-  )
-}
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a href={`#${item}`} key={item} variants={itemVariants}>
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
 
-export default Links
+export default Links;
