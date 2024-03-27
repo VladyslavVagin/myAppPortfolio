cc82caa1714b7d2b4fb026c428551d97a463f8d2
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-27 12:27:34 +0100

    progressBar
    portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,62 @@
 // @ts-nocheck
-import "./portfolio.scss"
+import { motion, useScroll, useSpring } from "framer-motion";
+import "./portfolio.scss";
+import { useRef } from "react";
 
 const items = [
-    {
-        id: 1,
-        title: "Drink Master",
-        img: "",
-        desc: ""
-    }
-]
+  {
+    id: 1,
+    title: "Drink Master",
+    img: "/public/portfolio/DrinkMaster_Desktop.jpg",
+    desc: "Application for add and create Cocktails' or Drinks' Recipes. User should login first by confirming email. Then he can change name, avatar or dark/light theme (by default dark). On home page shows the latest drinks from each categories of drinks, on page Drinks user can find some drink or cocktail by using filters. On page Add drink user can create his own cocktail or drink, and look on 4 most popular drinks which all users added to Favorites. Also have pages with own cocktails and cocktails which user was added to Favorites.",
+  },
+  {
+    id: 2,
+    title: "DrukArmy",
+    img: "/public/portfolio/ДрукАрмія.jpg",
+    desc: "The frontend part for the volunteer project PrintArmy, which is involved in 3D printing products for Ukrainian military personnel and medics, manages the ordering and delivery process of products to customers.",
+  },
+  {
+    id: 3,
+    title: "BookShelf",
+    img: "/public/portfolio/bookshelf_linkedn.jpg",
+    desc: "Web application of Book Library. For posibility add/remove books from Favourites user should Sign Up first. Application based on API requests. Mobile First approach, adaptive/responsive design, using Firebase Authentification.",
+  },
+  {
+    id: 4,
+    title: "Focus-Frame",
+    img: "/public/portfolio/focus-frame_linkedn.jpg",
+    desc: "Landing-page website for school of photography. Using Mobile First, responsive/adaptive user interface, optimized for most types of screens and animation.",
+  },
+];
+
+const Single = ({ item }) => {
+  return <section>{item.title}</section>;
+};
 
 const Portfolio = () => {
-  return (
-    <div className="portfolio">Portfolio</div>
-  )
-}
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
 
-export default Portfolio
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h2>Featured Works</h2>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
