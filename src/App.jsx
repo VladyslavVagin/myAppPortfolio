f436302d3e0bd608ce98d2c98efc2eb99662a2da
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-27 17:35:34 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import "./app.scss";
+// @ts-nocheck
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
 
 const App = () => {
   return (
@@ -23,7 +24,7 @@ const App = () => {
         <Parallax type="portfolio" />
       </section>
       <Portfolio />
-      <section id="Contact">Contact</section>
+      <section id="Contact"><Contact/></section>
       {/* <Test/> */}
     </div>
   );
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import "./portfolio.scss";
 import { useRef } from "react";
 
@@ -31,7 +31,29 @@ const items = [
 ];
 
 const Single = ({ item }) => {
-  return <section>{item.title}</section>;
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt={item.title} />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 const Portfolio = () => {
