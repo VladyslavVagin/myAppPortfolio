295f085d79815d4e622976b6772d9873f608de80
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-27 22:22:13 +0100

    add mobile

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -30,7 +30,7 @@ const Services = () => {
       variants={variants}
       initial="initial"
       ref={ref}
-      animate={isInView && "animate"}
+      animate={"animate"}
     >
       <motion.div className="textContainer" variants={variants}>
         <p>
