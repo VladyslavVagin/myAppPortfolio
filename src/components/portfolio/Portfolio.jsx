2a61b848ad176b45f511f64fc155e7db4aab6c7e
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-29 08:13:45 +0100

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,25 +7,25 @@ const items = [
   {
     id: 1,
     title: "Drink Master",
-    img: "/portfolio/DrinkMaster_Desktop.jpg",
+    img: "/portfolio/DrinkMaster.jpg",
     desc: "Team Project. Created on Node.js, React and MongoDB. Application for create own drinks and cocktails. My section: Header, Popular drinks, Switcher, React Routes, Modals for User Menu, Mobile Menu, Change avatar and name of user. Backend: create routes for update user and get popular drinks.",
   },
   {
     id: 2,
     title: "DrukArmy",
-    img: "/portfolio/ДрукАрмія.jpg",
+    img: "/portfolio/DrukArmiya.jpg",
     desc: "Project created on Next.js. The frontend part for the volunteer project PrintArmy, which is involved in 3D printing products for Ukrainian military personnel and medics, manages the ordering and delivery process of products to customers. My sections: Header, Footer, Main page, Terms, 404 Page, switcher, i18n multilanguage, mobile menu and modals for video and createdBy.",
   },
   {
     id: 3,
     title: "BookShelf",
-    img: "/portfolio/bookshelf_linkedn.jpg",
+    img: "/portfolio/bookshelf-2.jpg",
     desc: "Team Project. Web application of Book Library. Created on JavaScript and Vite bundler, based on API requests. For authentification use Firebase. My section: Bestsellers Books page, Categories, Authentification, Switcher, Pagination, Deleting books from Favorites.",
   },
   {
     id: 4,
     title: "Focus-Frame",
-    img: "/portfolio/focus-frame_linkedn.jpg",
+    img: "/portfolio/focus-frame.jpg",
     desc: "Team project. Landing page for school of photography. Created with HTML5, CSS3 and JavaScript (for animation). My section: Hero, Advertisment, Mobile menu, About Us.",
   },
 ];
